efbca65e0923cfec945e7aa4c636c4f9363f981f
nayakrachana28 <nayakrachana28@gmail.com>
2022-09-27 12:48:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_CSharp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Console.WriteLine("Hello");
+            two_array();
+        }
+
+        public static void two_array()
+        {
+            int[,] array1 = new int[,] 
+            { 
+                {1, 2},
+                {3, 4}
+            };
+
+            for(int i = 0; i < array1.GetLength(0); i++)
+            {
+                for(int j = 0; j < array1.GetLength(1); j++)
+                {
+                    Console.WriteLine(array1[i,j]);
+                }
+            }
+        }
+    }
+}
+
